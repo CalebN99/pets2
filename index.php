2972f9ee0d214de0c252948d29784f00bf6c7498
Caleb Norris <norrisc129@gmail.com>
2022-05-05 12:17:30 -0400

    Composer and controller setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo "My pets";
+});
+
+//Run f3
+$f3->run();
